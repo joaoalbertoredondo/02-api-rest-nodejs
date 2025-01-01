bd912f8ea00f6a3d3e8abcd55b099f0e5079642c
João Alberto Redondo <169093784+joaoalbertoredondo@users.noreply.github.com>
2025-01-01 11:43:42 -0300

    class: banco de dados

--- src/server.ts
+++ src/server.ts
@@ -1,17 +1,19 @@
 import fastify from 'fastify'
+import crypto from 'node:crypto'
 import { knex } from './database'
+import { env } from './env'
 
 const app = fastify()
 
 app.get('/hello', async () => {
-  const tables = await knex('sqlite_schema').select('*')
+  const transactions = await knex('transactions').select('*')
 
-  return tables
+  return transactions
 })
 
 app
   .listen({
-    port: 3333,
+    port: env.PORT,
   })
   .then(() => {
     console.log('HTTP server running!')
